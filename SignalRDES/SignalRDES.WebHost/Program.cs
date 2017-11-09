1ff523bf2e49c8a91e7b2fd585ba6e5af071a902
Zbigniew Marszolik <z.marszolik@gmail.com>
2017-11-09 15:33:30 +0100

    SignalR solution created.

---
+++ SignalRDES/SignalRDES.WebHost/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SignalRDES.WebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Signal R");
+            Console.ReadLine();
+        }
+    }
+}
